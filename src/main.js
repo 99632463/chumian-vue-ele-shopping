008098b7e9ca1ca092a3adb961ca2178d8cd0017
kai.wang <99632463@qq.com>
2021-04-09 22:44:04 +0800

    test4

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import $conf from './common/config/config'
 import directives from './common/directives'
 import store from './store'
 
-console.log('test3');
+console.log('test4');
 
 import VueDND from 'awe-dnd'
 Vue.use(VueDND)
